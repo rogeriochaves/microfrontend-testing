d41f97b4702b8bd9fb3909313e5bca2a6fee0471
Rogerio Chaves <rogeriocfj+github@gmail.com>
2016-12-05 22:14:30 -0300

    Use script tags to load components

--- server.js
+++ server.js
@@ -1,20 +1,28 @@
 const express = require('express');
+const fs = require('fs');
+const path = require('path');
 let app = express();
 
+const jsRender = (file) => {
+  const html = fs.readFileSync(path.join(__dirname, file));
+  const safeHtml = html.toString().replace(/\n/g, '').replace(/'/g, "\\'");
+  return `debugger; document.write('${safeHtml}')`;
+};
+
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/index.html');
 });
 
-app.get('/header', (req, res) => {
-    setTimeout(() => res.sendFile(__dirname + '/header.html'), 500);
+app.get('/header.js', (req, res) => {
+  setTimeout(() => res.send(jsRender('header.html')), 500);
 });
 
-app.get('/content', (req, res) => {
-    setTimeout(() => res.sendFile(__dirname + '/content.html'), 3000);
+app.get('/content.js', (req, res) => {
+  setTimeout(() => res.send(jsRender('content.html')), 3000);
 });
 
-app.get('/footer', (req, res) => {
-    setTimeout(() => res.sendFile(__dirname + '/footer.html'), 1000);
+app.get('/footer.js', (req, res) => {
+  setTimeout(() => res.send(jsRender('footer.html')), 1000);
 });
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
